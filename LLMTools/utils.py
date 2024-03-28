aea77e30f93cf28c2518ef43e4f79e9976c5c72b
johnwick123f <128541091+johnwick123f@users.noreply.github.com>
2024-03-28 11:56:16 -0400

    Update utils.py

--- LLMTools/utils.py
+++ LLMTools/utils.py
@@ -1,12 +1,38 @@
 from llama_cpp import Llama
-# loads model
-def load_model(model_path, gpu_layers=-1, ctx=2048):
-    llm = Llama(
-          model_path=model_path,
-          n_gpu_layers=gpu_layers
-          n_ctx=ctx,
-    )
-    return llm
+from LLMTools.prompts import main_llm_prompt
+class llm:
+    def __init__(self, model_path, gpu_layers=-1, ctx=2048):
+        self.language_model = llm = Llama(model_path=model_path, n_gpu_layers=gpu_layers n_ctx=ctx)
+
+    def generate(self, prompt, stop, stream=True, max_tokens=300, temp=0.2):
+        if stream == False:
+            out = llm(prompt, max_tokens=max_tokens, stop=stop, temperature=temp)
+            return out["choices"][0]["text"]
+        else:
+            for out in llm(prompt, max_tokens=max_tokens, stop=stop, temperature=temp, stream=True):
+                yield out["choices"][0]["text"]
+
+    def infer(self, prompt):
+        full_out = ""
+        out = ""
+        while True:
+            for token in normal_generate(prompt, stream=True):
+                if "Observ" in token: 
+                    answer = "True"
+                    prompt += f"Observation: {answer}\nThought:"
+                    full_out += f"Observation: {answer}\nThought:"
+                    break
+                elif "Final Answer:" in full_out:
+                    out += token
+                else:
+                    prompt += token
+                    full_out += token
+                    #print(token, end="")
+            if "Final Answer" in full_out: 
+                break
+            else:
+                pass
+
 ## function for generating normally without chat. Important for function calling.
 def normal_generate(prompt, stream=False, stop=None):
     output = llm(prompt, stop=stop)
