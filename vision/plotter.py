f5f8f06d8060287fa67c2b60bf917739199ffb1a
johnwick123f <128541091+johnwick123f@users.noreply.github.com>
2024-04-14 14:07:01 -0400

    Update plotter.py

--- vision/plotter.py
+++ vision/plotter.py
@@ -1,18 +1,19 @@
 import sympy as sp
-import matplotlib.pyplot as plt
 import numpy as np
+from PIL import Image, ImageDraw
 
-def plot_equation(equation, xlim=None, ylim=None):
+def plot_equation_pil(equation, size=(400, 400), xlim=None, ylim=None):
     """
-    Plot the graph of a given equation.
+    Plot the graph of a given equation and return it as a PIL image.
 
     Parameters:
     - equation: str, the equation in the form 'y = f(x)'.
+    - size: tuple, size of the output image (default is (400, 400)).
     - xlim: tuple, limits for the x-axis (default is None).
     - ylim: tuple, limits for the y-axis (default is None).
 
     Returns:
-    - None
+    - PIL Image
     """
 
     # Parse the equation
@@ -23,27 +24,27 @@ def plot_equation(equation, xlim=None, ylim=None):
     func = sp.lambdify(x, y, modules=['numpy'])
 
     # Generate x values
-    x_values = np.linspace(-10, 10, 400)
+    x_values = np.linspace(-10, 10, size[0])
 
     # Calculate y values
     y_values = func(x_values)
 
+    # Normalize y values to fit within the image height
+    min_y, max_y = min(y_values), max(y_values)
+    y_values = (y_values - min_y) / (max_y - min_y) * size[1]
+
+    # Create a blank white image
+    image = Image.new('RGB', size, 'white')
+    draw = ImageDraw.Draw(image)
+
     # Plot the graph
-    plt.plot(x_values, y_values)
+    for i in range(len(x_values) - 1):
+        draw.line([(i, size[1] - int(y_values[i])), ((i + 1), size[1] - int(y_values[i + 1]))], fill='black')
 
     # Add labels and title
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.title(f'Plot of {equation}')
+    draw.text((size[0] // 2, size[1] - 20), 'x', fill='black')
+    draw.text((size[0] - 20, size[1] // 2), 'y', fill='black')
+    draw.text((10, 10), f'Plot of {equation}', fill='black')
 
-    # Set axis limits if specified
-    if xlim:
-        plt.xlim(xlim)
-    if ylim:
-        plt.ylim(ylim)
-
-    # Show the plot
-    plt.grid(True)
-    plt.axhline(0, color='black', linewidth=0.5)
-    plt.axvline(0, color='black', linewidth=0.5)
-    plt.show()
+    # Return the image
+    return image
